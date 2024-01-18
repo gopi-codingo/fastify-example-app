30691c410a5fef254ed2816b04a63f17c1ceac82
Gopikrishna L <95208821+gopi-codingo@users.noreply.github.com>
2024-01-18 13:12:56 +0000

    init

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello CodeSandbox");
+import Fastify from 'fastify';
+
+const fastify = Fastify({
+    logger: true,
+});
+
+fastify.get('/', function (request, reply) {
+    reply.send({ hello: 'world'});
+});
+
+fastify.listen({ port: 3100 }, function (err, address) {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+});
